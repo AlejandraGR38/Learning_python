6973ec4b3492fc9ae5f9926235500fce019ab44d
Alejandra Gomez <alejandra.gomez3852@gmail.com>
2020-05-25 16:20:15 -0500

    Some changes

--- project1.py
+++ project1.py
@@ -1,7 +1,6 @@
 credit_principal = 'Credit principal: 1000'
 final_output = 'The credit has been repaid!'
 first_month = 'Month 1: paid out 250'
-# Months
 second_month = 'Month 2: paid out 250'
 third_month = 'Month 3: paid out 500'
 
@@ -10,3 +9,6 @@ print(credit_principal)
 print(second_month)
 print(third_month)
 print(final_output)
+
+
+#Final
